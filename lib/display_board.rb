bf30fb9b7472af425c4facb86778f8ad09578d47
Zach Bayard <zbayard@gmail.com>
2020-09-24 18:58:17 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,3 +1,7 @@
 def display_board(board = [" "," "," "," "," "," "," "," "," "])
-  
-# out the current state.
+  puts " #{board[0]}  | #{board[1]}  | #{board[2]}  "
+  puts "-----------"
+  puts " #{board[4]}  | #{board[5]}  | #{board[6]}  "
+  puts "-----------"
+  puts " #{board[4]}  | #{board[5]}  | #{board[6]}  "
+end
