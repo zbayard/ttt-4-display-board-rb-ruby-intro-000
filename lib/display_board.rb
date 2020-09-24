a4f146f02ac5224fbc64c1529e33a86710f28961
Zach Bayard <zbayard@gmail.com>
2020-09-24 19:16:52 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,5 +5,5 @@ def display_board(board)
   puts "-----------"
   puts " #{board[4]} | #{board[5]} | #{board[6]} "
   puts "-----------"
-  puts " #{board[7]} | #{board[8]} | #{board[9]}  "
+  puts " #{board[7]} | #{board[8]} | #{board[9]} "
 end
